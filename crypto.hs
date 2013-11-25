36233addd5a3b773af1dc20b8b5690e7b1677c2d
Baldur Blöndal <baldur@student.chalmers.se>
2013-11-25 08:48:36 +0100

    completed

--- crypto.hs
+++ crypto.hs
@@ -1,45 +1,21 @@
-{-# LANGUAGE  BangPatterns #-}
-
--- First comment!
-
 module Main where
 
-import System.Random
-import Text.Printf
-import Control.Monad
-import Test.QuickCheck
-import Control.DeepSeq
-import System.IO
+import Control.Applicative ((<$>))
+import Control.Monad       (replicateM_)
+import System.IO           (hSetBuffering, stdout, BufferMode(LineBuffering))
+import System.IO.Unsafe    (unsafePerformIO)
+import System.Random       (newStdGen, getStdGen, randomRIO, randomRs, randoms)
+import Text.Printf         (printf)
 
-data ParameterTuple = ParameterTuple Integer Integer Integer deriving Show
+data ParameterTuple = ParameterTuple {
+     p :: Integer, q :: Integer, g :: Integer
+  } deriving Show
 
--- Smart constructor for ParameterTuple
-
-parameterTuple :: Integer -> Integer -> Integer -> Maybe ParameterTuple
-parameterTuple p q g
-  | and [isPrime p,             -- p and q must be prime
-         isPrime q,
-         p `bitNumber` 1024,    -- p must be a 1024-bit number
-         q `bitNumber` 160,     -- q must be a 160-bit number
-         q `isDivisor` (p-1),   -- q is a divisor of p-1
-         (g `hasOrder` q) p]    -- g has order q
-         = Just (ParameterTuple p q g)
-  | otherwise = Nothing
-
-
-q `bitNumber` bits = 0 <= q && q < 2^(bits)
-q `isDivisor` p = p `mod` q == 0
-(g `hasOrder` q) p = g > 1 && powm g q p 1 == 1
-
--- TODO: Implement
-isPrime p = True
-
-type PubKey = Integer
-
-type Keypair = (Integer, PubKey)
-
-type Message = Integer
-type Signature = (Integer, Integer)
+type PubKey       = Integer
+type Keypair      = (Integer, PubKey)
+type Message      = Integer
+type Signature    = (Integer, Integer)
+type SecretNumber = Integer
 
 data Accepted = ValidSignature | InvalidSignature 
 
@@ -47,161 +23,200 @@ instance Show Accepted where
   show ValidSignature   = "signature_valid"
   show InvalidSignature = "signature_invalid"
 
-type SecretNumber = Integer
+-- Smart constructor for ParameterTuple
+parameterTuple :: Integer -> Integer -> Integer -> Maybe ParameterTuple
+parameterTuple p q g
+  | and [p `bitNumber` 1024,    -- p must be a 1024-bit number
+         q `bitNumber` 160,     -- q must be a 160-bit number
+         q `isDivisor` (p-1),   -- q is a divisor of p-1
+         (g `hasOrder` q) p,    -- g has order q
+         isPrime p,             -- p and q must be prime
+         isPrime q] = Just (ParameterTuple p q g)
+  | otherwise       = Nothing
+
+-- (q `bitNumbers` bits) returns True if ‘q’ has has between ‘bits-1’
+-- and ‘bits’ bits.
+bitNumber :: Integer -> Integer -> Bool
+q `bitNumber` bits = q >= 2^(bits-1) && q < 2^bits
+
+isDivisor :: Integer -> Integer -> Bool
+q `isDivisor` p = p `mod` q == 0
+
+hasOrder :: Integer -> Integer -> Integer -> Bool
+(g `hasOrder` q) p = g > 1 && powerMod g q p == 1
 
 -- Computation of inverse value (Appendix C.1)
--- multInv a n ≡ a⁻¹ mod n
-multInv :: Integer -> Integer -> Integer
-multInv a n = case gcdE n a of
+-- modInv a n ≡ a⁻¹ mod n
+modInv :: Integer -> Integer -> Integer
+modInv a n = case gcdE n a of
   (1, s, t) -> (t + n) `mod` n
-  _         -> error "No inverse exists"
+  _         -> error "modInv: No inverse exists"
 
-powm :: Integer -> Integer -> Integer -> Integer -> Integer
-powm b 0 m r = r
-powm b e m r | e `mod` 2 == 1 = powm (b * b `mod` m) (e `div` 2) m (r * b `mod` m)
-powm b e m r = powm (b * b `mod` m) (e `div` 2) m r
-
-prop_multInv (Positive a) (Positive n) = coprime a n && a > 1 && n > 1 ==> let
-  inverse = multInv a n
-  in inverse == 0 || inverse * a == 1
-
-coprime a n = (a `gcd` n) == 1
-
-prop_between (Positive a) (Positive mod) =
-  coprime a mod ==> let
-    inverse = (a `multInv` mod)
-    in inverse == 0 || 0 < inverse && inverse < mod
-
-gcdE' a 0 = (a, 1, 0)
-gcdE' a b = (d, t, s - q*t) where
+-- Taken from slides.
+gcdE :: Integer -> Integer -> (Integer, Integer, Integer)
+gcdE a 0 = (a, 1, 0)
+gcdE a b = (d, t, s - q*t) where
   (q, r)    = a `quotRem` b
-  (d, s, t) = gcdE' b r
-
-gcdE :: Integral a => a -> a -> (a, a, a)
-gcdE a b = (d, u, v)
-  where
-    (d, x, y) = eGCD 0 1 1 0 (abs a) (abs b)
-    u | a < 0     = negate x
-      | otherwise = x
-    v | b < 0     = negate y
-      | otherwise = y
-    eGCD !n1 o1 !n2 o2 r s
-      | s == 0    = (r, o1, o2)
-      | otherwise = case r `quotRem` s of
-                      (q, t) -> eGCD (o1 - q*n1) n1 (o2 - q*n2) n2 s t
-
--- TODO: Use a new key if either r = 0 or s = 0.
-sign :: ParameterTuple -> Keypair -> Message -> [SecretNumber] -> Signature
-sign (ParameterTuple p q g) (x, y) msg ks = (r, s) where
-  k = head ks
-  r = (powm g k p 1) `mod` q
-  z = msg
-  s = ((k `multInv` q) * (z + x * r `mod` q)) `mod` q
-
-verify :: ParameterTuple -> PubKey -> Message -> Signature -> Accepted
-verify (ParameterTuple p q g) y msg sig@(r, s)
-  | not (0 < r && r < q) = InvalidSignature
-  | not (0 < s && s < q) = InvalidSignature
-  | otherwise            = if v == r
-                           then ValidSignature
-                           else InvalidSignature
-    where
-        w  = s `multInv` q
-        z  = msg
-        u₁ = (z*w) `mod` q
-        u₂ = (r*w) `mod` q
-        v  = ((powm g u₁ p 1) * (powm y u₂ p 1) `mod` p) `mod` q
-
-main = do
-  hSetBuffering stdout LineBuffering
-
-  'p':'=':p' <- getLine
-  'q':'=':q' <- getLine
-  'g':'=':g' <- getLine
-
-  let p, q, g :: Integer
-      [p, q, g] = map read [p', q', g']
-
-  case parameterTuple p q g of
-    Nothing -> invalid
-    Just param  -> do
-      valid
-      action <- getLine
-      case action of
-        "genkey" -> genkeys param
-        "sign"   -> signFromInput param
-        "verify" -> verifyFromInput param
-        
-  where
-    invalid = putStrLn "invalid_group"
-    valid   = putStrLn "valid_group"
-
-    genkeys param = do
-      'n':'=':n <- getLine
-      replicateM_ (read n) (genkey param)
-
-    genkey (ParameterTuple p q g) = do
-      x <- genPrivateKey q
-
-      let y = powm g x p 1
-
-      printf "x=%d\n" x
-      printf "y=%d\n" y
-      
-    signFromInput param = do
-      'x':'=':x <- getLine
-      'y':'=':y <- getLine
-      foo <- getContents
-      mapM_ (generateSignature param (read x, read y)) (lines foo)
-
-    generateSignature param keypair ('D':'=':d) = do
-
-      seed <- getStdGen
-
-      let digest = read $ "0x" ++ d
-
-      let signature@(r, s) = sign param keypair digest (map abs (randoms seed))
-
-
-      printf "r=%d\n" r
-      printf "s=%d\n" s
-    
-    verifyFromInput param = do
-      'y':'=':y <- getLine
-
-      input <- getContents
-      mapM_ (verifySingleInput param y) (chunk3 (lines input))
-
-    verifySingleInput param@(ParameterTuple p q g) publicKey inputs = case inputs of
-      ['D':'=':digest,
-       'r':'=':r,
-       's':'=':s] -> do
-         
-       
-         let signature = verify param (read publicKey) (read $ "0x" ++ digest) (read r, read s)
-         print signature
-
-    chunk3 (a:b:c:rest) = [a,b,c]:chunk3 rest
-    chunk3 _ = []
-
+  (d, s, t) = gcdE b r
 
 -- x is the private key which must remain secret
 -- It is a (pseudo)randomly generated Integer s.t. 0 < x < q: [1, q-1]
 genPrivateKey :: Integer -> IO Integer
 genPrivateKey q = randomRIO (1, q-1)
 
-chunk3 (a:b:c:rest) = [a,b,c]:chunk3 rest
-chunk3 _ = []
+-- Signs message.
+sign :: ParameterTuple -> Keypair -> Message -> [SecretNumber] -> Signature
+sign param@(ParameterTuple p q g) (x, y) z (k:ks)
+  | r == 0 || s == 0 = sign param (x, y) z ks
+  | otherwise        = (r, s)
+  where
+    r, s :: Integer 
+    r = (powerMod g k p) `mod` q
+
+    s = ((k `modInv` q) * (z + x * r `mod` q)) `mod` q
+
+-- Verifies signature.
+verify :: ParameterTuple -> PubKey -> Message -> Signature -> Accepted
+verify (ParameterTuple p q g) y z sig@(r, s)
+  | and [ 0 < r, r < q,
+          0 < s, s < q,
+          v == r] = ValidSignature
+  | otherwise     = InvalidSignature
+    where
+        u₁, u₂, w, v :: Integer
+        (u₁, u₂) = ((z*w) `mod` q, (r*w) `mod` q)
+        
+        w        = s `modInv` q
+        
+        v        = ((powerMod g u₁ p) * (powerMod y u₂ p) `mod` p) `mod` q
+
+main :: IO ()
+main = do
+  hSetBuffering stdout LineBuffering
+
+  -- Form parameter tuple.
+  'p':'=': p <- getLine
+  'q':'=': q <- getLine
+  'g':'=': g <- getLine
+
+  let param = parameterTuple (read p) (read q) (read g) 
+
+  case param of
+    Nothing     -> putStrLn "invalid_group"
+    Just param  -> do
+      putStrLn "valid_group"
+      action <- getLine
+      case action of
+        "genkey" -> genkeys         param
+        "sign"   -> signFromInput   param
+        "verify" -> verifyFromInput param
+
+  where
+
+    genkeys param = do
+      'n':'=': n <- getLine
+      replicateM_ (read n) (genkey param)
+
+    genkey (ParameterTuple p q g) = do
+      x <- genPrivateKey q
+      
+      let y = powerMod g x p
+
+      printf "x=%d\n" x
+      printf "y=%d\n" y
+      
+    signFromInput param = do
+    
+      'x':'=': x <- getLine
+      'y':'=': y <- getLine
+      input      <- lines <$> getContents
+      
+      mapM_ (generateSignature param (read x, read y)) input
+
+    generateSignature param keypair ('D':'=':d) = do
+
+      seed <- getStdGen
+
+      let digest = readHex d
+
+      let signature@(r, s) = sign param keypair digest (map abs (randoms seed))
+
+      printf "r=%d\n" r
+      printf "s=%d\n" s
+    
+    verifyFromInput param = do
+      'y':'=': y <- getLine
+      input      <- lines <$> getContents
+      
+      mapM_ (verifySingleInput param y) (chunk3 input)
+
+    verifySingleInput param@(ParameterTuple p q g) publicKey inputs = case inputs of
+      ['D':'=':d,
+       'r':'=':r,
+       's':'=':s] -> do
+
+         let digest    = readHex d
+             signature = verify param (read publicKey) digest (read r, read s)
+         print signature
+
+    chunk3 :: [a] -> [[a]]
+    chunk3 (a:b:c:rest) = [a,b,c]:chunk3 rest
+    chunk3 _            = []
+
+    readHex :: String -> Integer
+    readHex digest = read ("0x" ++ digest)
 
 
-p=102865584259843077175583195011997798900482038016705824136288380475734860009055428071534495956844807748416572686838253895244634687898659646424515259679129905513743899853971066468883670407530107234961085482225328667572772611162756643027105617873895021996158552984843708233824989792811721408577351617080369547993
-q=734415599462729831694143846331445277609193755927
-g=63615006880335642768473038477258757436464860136916565207798584167060621564899979263408565137993978149206751054438974059615983337126379668370747907507911540381031959187353048278562320341063050939775344313271013777131358834376209974551749493023310606751625276738876397935042130121966817767949476523717161640453
+-- Some QuickCheck
 
-Just param = parameterTuple p q g
+-- prop_modInv (Positive a) (Positive n) = coprime a n && a > 1 && n > 1 ==> let
+--   inverse = modInv a n
+--   in inverse == 0 || inverse * a == 1
 
-y=1099906791313925528746008054081768734007884349815325963667520491768596235922636596649198172987598573083011790017146356061273962023338014420645127092468263770753970716461208880423045761205934804880887634821616587683235765408867072852094816664326084550730344050243082288308837441908172297994552279650972016922
-digest=read "0x10B4D55F2376DBA00CE4A6AE2B122E9554035EF2" :: Integer
-r=497727687827108870230917469165124644171957997527
-s=69924200561536940344114164706214298822631922629
-signature = (r, s)
+-- coprime a n = (a `gcd` n) == 1
+
+-- prop_between (Positive a) (Positive mod) =
+--   coprime a mod ==> let
+--     inverse = (a `modInv` mod)
+--     in inverse == 0 || 0 < inverse && inverse < mod
+
+-- Taken from: http://rosettacode.org/wiki/Miller-Rabin_primality_test#Haskell
+
+-- Miller-Rabin wrapped up as an (almost deterministic) pure function
+isPrime :: Integer -> Bool
+isPrime n = unsafePerformIO (isMillerRabinPrime 100 n)
+
+isMillerRabinPrime :: Int -> Integer -> IO Bool
+isMillerRabinPrime k n
+   | even n    = return (n == 2)
+   | n < 100   = return (n `elem` primesTo100)
+   | otherwise = do ws <- witnesses k n
+                    return $ and [test n (pred n) evens (head odds) a | a <- ws]
+  where
+    (evens, odds) = span even (iterate (`div` 2) (pred n))
+ 
+primesTo100 :: [Integer]
+primesTo100 = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
+
+test :: Integer -> Integer ->  [Integer] -> Integer -> Integer -> Bool
+test n n_1 evens d a = x `elem` [1,n_1] || n_1 `elem` powers 
+  where
+    x      = powerMod a d n
+    powers = map (\g -> powerMod a g n) evens
+ 
+witnesses :: Int -> Integer -> IO [Integer]
+witnesses k n 
+  | n < 9080191         = return [31,73]
+  | n < 4759123141      = return [2,7,61]
+  | n < 3474749660383   = return [2,3,5,7,11,13]
+  | n < 341550071728321 = return [2,3,5,7,11,13,17]
+  | otherwise           = do g <- newStdGen
+                             return $ take k (randomRs (2,n-1) g)
+
+-- powerMod x n m = x^n `mod` m
+powerMod :: Integer -> Integer -> Integer -> Integer
+powerMod x n m = f (n - 1) x x `rem` m 
+  where
+  f d a y = if d == 0 then y else g d a y 
+  g i b y | even i    = g (i `quot` 2) (b*b `rem` m) y
+          | otherwise = f (i-1) b (b*y `rem` m)
